f8f50e8592d3af66348ea040f9e378656820e38d
qwert qwert <Dominikch19@gmail.com>
2023-11-04 13:06:49 +0100

    many things

--- lab5/Form1.Designer.cs
+++ lab5/Form1.Designer.cs
@@ -29,13 +29,168 @@ namespace lab5
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.odczytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dekoratorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kompresjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.szyfrowanieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editField = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.dekoratorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zapiszToolStripMenuItem,
+            this.odczytajToolStripMenuItem,
+            this.koniecToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // odczytajToolStripMenuItem
+            // 
+            this.odczytajToolStripMenuItem.Name = "odczytajToolStripMenuItem";
+            this.odczytajToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.odczytajToolStripMenuItem.Text = "Odczytaj";
+            this.odczytajToolStripMenuItem.Click += new System.EventHandler(this.odczytajToolStripMenuItem_Click);
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.koniecToolStripMenuItem.Text = "Koniec";
+            this.koniecToolStripMenuItem.Click += new System.EventHandler(this.koniecToolStripMenuItem_Click);
+            // 
+            // dekoratorToolStripMenuItem
+            // 
+            this.dekoratorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kompresjaToolStripMenuItem,
+            this.szyfrowanieToolStripMenuItem});
+            this.dekoratorToolStripMenuItem.Name = "dekoratorToolStripMenuItem";
+            this.dekoratorToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.dekoratorToolStripMenuItem.Text = "Dekorator";
+            // 
+            // kompresjaToolStripMenuItem
+            // 
+            this.kompresjaToolStripMenuItem.CheckOnClick = true;
+            this.kompresjaToolStripMenuItem.Name = "kompresjaToolStripMenuItem";
+            this.kompresjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kompresjaToolStripMenuItem.Text = "Kompresja";
+            this.kompresjaToolStripMenuItem.Click += new System.EventHandler(this.kompresjaToolStripMenuItem_Click);
+            // 
+            // szyfrowanieToolStripMenuItem
+            // 
+            this.szyfrowanieToolStripMenuItem.CheckOnClick = true;
+            this.szyfrowanieToolStripMenuItem.Name = "szyfrowanieToolStripMenuItem";
+            this.szyfrowanieToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.szyfrowanieToolStripMenuItem.Text = "Szyfrowanie";
+            this.szyfrowanieToolStripMenuItem.Click += new System.EventHandler(this.szyfrowanieToolStripMenuItem_Click);
+            // 
+            // editField
+            // 
+            this.editField.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.editField.Location = new System.Drawing.Point(0, 46);
+            this.editField.MaximumSize = new System.Drawing.Size(2000, 2000);
+            this.editField.Multiline = true;
+            this.editField.Name = "editField";
+            this.editField.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.editField.Size = new System.Drawing.Size(800, 423);
+            this.editField.TabIndex = 2;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pathTextBox.Location = new System.Drawing.Point(63, 27);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(665, 20);
+            this.pathTextBox.TabIndex = 3;
+            this.pathTextBox.TextChanged += new System.EventHandler(this.pathTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ścieżka:";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(725, 26);
+            this.searchButton.MaximumSize = new System.Drawing.Size(75, 20);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 20);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Szukaj";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.editField);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Edytor tekstu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem odczytajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dekoratorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kompresjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem szyfrowanieToolStripMenuItem;
+        private System.Windows.Forms.TextBox editField;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button searchButton;
     }
 }
 
--- lab5/Form1.cs
+++ lab5/Form1.cs
@@ -7,14 +7,179 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
 
 namespace lab5
 {
     public partial class Form1 : Form
     {
+        private bool compression = false;
+        private bool encryption = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void koniecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveToFile(editField.Text, pathTextBox.Text);
+        }
+
+        private void saveToFile(string text, string path)
+        {
+            try
+            {
+                using (FileStream file = new FileStream(path, FileMode.Create))
+                {
+                    Stream finalStream = file;
+                    if (encryption)
+                    {
+                        using (SHA256 sha = SHA256.Create())
+                        {
+                            using (Aes aesAlg = Aes.Create())
+                            {
+                                byte[] IV = new byte[aesAlg.BlockSize / 8]; ;
+                                byte[] Key = Encoding.UTF8.GetBytes("YourEncryptionKey");
+                                byte[] hashKey = sha.ComputeHash(Key);
+                                aesAlg.Key = hashKey;
+                                aesAlg.IV = IV;
+                                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                                finalStream = new CryptoStream(finalStream, encryptor, CryptoStreamMode.Write);
+                            }
+                        }
+                    }
+
+                    if (compression)
+                    {
+                        finalStream = new GZipStream(finalStream, CompressionMode.Compress);
+                    }
+
+                    byte[] bytes = Encoding.UTF8.GetBytes(text); ;
+                    finalStream.Write(bytes, 0, bytes.Length);
+                    finalStream.Dispose();
+                }
+            }
+            catch { 
+                
+            }
+            
+
+        }
+        
+
+        private String readFile(string path)
+        {
+            byte[] buffer;
+            try
+            {
+                using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    Stream finalStream = file;
+                    if (encryption)
+                    {
+                        using (SHA256 sha = SHA256.Create())
+                        {
+                            using (Aes aesAlg = Aes.Create())
+                            {
+                                byte[] IV = new byte[aesAlg.BlockSize / 8]; ;
+                                byte[] Key = Encoding.UTF8.GetBytes("YourEncryptionKey");
+                                byte[] hashKey = sha.ComputeHash(Key);
+                                aesAlg.Key = hashKey;
+                                aesAlg.IV = IV;
+                                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                                finalStream = new CryptoStream(finalStream, decryptor, CryptoStreamMode.Read);
+                            }
+                        }
+                    }
+
+                    if (compression)
+                    {
+                        finalStream = new GZipStream(finalStream, CompressionMode.Decompress);
+                    }
+
+                    using (MemoryStream output = new MemoryStream())
+                    {
+                        try
+                        {
+                            finalStream.CopyTo(output);
+                            buffer = output.ToArray();
+                        }
+                        catch
+                        {
+                            return "Nie można odczytać pliku";
+                        }
+
+                    }
+                    finalStream.Dispose();
+                }
+            }
+            catch {
+                return "Nie można odczytać pliku";
+            }
+           
+
+            return Encoding.UTF8.GetString(buffer);
+        }
+
+        private void odczytajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            editField.Text = readFile(pathTextBox.Text);
+        }
+
+        private void oDekoratorachToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            sender.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pathTextBox_TextChanged(object sender, EventArgs e)
+        {         
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "C:\\";
+            //openFileDialog.Filter = "All Files (*.*)|*.*"; 
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                pathTextBox.Text = selectedFilePath;
+            }
+        }
+
+        private void kompresjaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //kompresjaToolStripMenuItem.Checked = !kompresjaToolStripMenuItem.Checked;
+            compression = kompresjaToolStripMenuItem.Checked;
+        }
+
+        private void szyfrowanieToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //szyfrowanieToolStripMenuItem.Checked = !szyfrowanieToolStripMenuItem.Checked;
+            encryption = szyfrowanieToolStripMenuItem.Checked;
         }
     }
 }
